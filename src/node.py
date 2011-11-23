51db33fb3dfe0f67c2a7a06f9f918cf04f7ed395
Marcus McCurdy <marcus.mccurdy@gmail.com>
2011-11-23 12:15:34 -0500

    adding skeleton source files

---
+++ src/node.py
@@ -0,0 +1,13 @@
+import hashlib
+
+class Node(object):
+    """Class that represents a node in the network"""
+    self.kBuckets = []
+    
+    def __init__(self):
+    	"""Initialize kBuckets and ?"""
+    
+    def find_node(self, nodeID):
+    	pass
+    
+    
