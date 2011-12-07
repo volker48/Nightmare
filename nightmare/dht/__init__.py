d68913d03c7b0758b3b30e57cf93e491aed9a5d6
Marcus McCurdy <marcus.mccurdy@gmail.com>
2011-12-07 15:09:48 -0500

    created a sub directory to hold packages

---
+++ nightmare/dht/__init__.py
@@ -0,0 +1,7 @@
+"""
+Nightmare: Scalable distributed PUB/SUB
+
+Nightmare DHT contains all the code for the underlying DHT protocal that
+Nightmare is built on top of. The current implementation uses the 
+Kademlia DHT protocol.
+"""
---
+++ nightmare/nightmare.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+def main(self):
+    pass
+
+if __name__ == "__main__":
+    main()
