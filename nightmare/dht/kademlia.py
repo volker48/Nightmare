258cb184ca2bf55d05b2d3c9da02f57a357d5abb
Marcus McCurdy <marcus.mccurdy@gmail.com>
2011-12-08 16:03:52 -0500

    working on classes and putting some logging in

--- nightmare/dht/kademlia.py
+++ nightmare/dht/kademlia.py
@@ -3,7 +3,8 @@ Classes and methods for the Kademlia DHT protocol.
 """
 
 class ContactInfo(object):
-    """Class that holds the contact information for a node in the Kademlia
+    """
+    Class that holds the contact information for a node in the Kademlia
     network. Instances of this class are stored in the k-buckets.
     """
 
@@ -15,3 +16,23 @@ class ContactInfo(object):
         self.port = port
         self.node_id = node_id
         self.last_seen = last_seen
+        
+    def __str__(self):
+        message = 'node<%s>@%s:%d last_seen: %s' 
+        return message % (self.node_id, self.ip, self.port, self.last_seen)
+    
+    def __repr__(self):
+        return str(self.__dict__)
+
+
+class KBucket(object):
+    """
+    Class to handle k-buckets.
+    """
+    
+    def __init__(self, k=20):
+        """
+        k --- The number of nodes to store in each KBucket defaults to 20
+        """
+        self.k = k
+        
