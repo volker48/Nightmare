872aaa1a453541466feabe28797981adea047dc9
Marcus McCurdy <marcus.mccurdy@gmail.com>
2011-12-12 08:07:14 -0500

    removed file main.py moved all the code into nightmare.py

--- nightmare/nightmare.py
+++ nightmare/nightmare.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
-def main(self):
-    pass
+
+import dht.kademlia
+import logging
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="Nightmare PUB/SUB")
+    parser.add_argument("--logfile", default="nightmare.log", 
+                    help="The name of the log file (default: nightmare.log)")
+    args = parser.parse_args()
+    logging.basicConfig(filename=args.logfile,level=logging.DEBUG)
+    logging.info("Staring Kademlia")
+    contact = dht.kademlia.ContactInfo("192.168.176.2", 56789, 1234)
+    print contact
+    print repr(contact)
 
 if __name__ == "__main__":
     main()
